6ab390559339d73158e56ed380d35ee083de9df9
German Diaz <grman.diaz@yahoo.com.ar>
2024-11-07 17:21:00 -0300

    deploy railway

--- src/conexion/database.js
+++ src/conexion/database.js
@@ -1,18 +1,27 @@
-require('dotenv').config();
-const { Sequelize } = require('sequelize');
+const dotenv = require("dotenv");
+const ENV = process.env.NODE_ENV || 'local';
+dotenv.config({ path: `.env.${ENV}` });
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PWD, {
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PWD,
+  {
     host: process.env.HOST,
-    dialect: 'mysql',  
-    dialectModule: require('mysql2'),  
-});
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    //dialectModule: require("mysql2"),
+  }
+);
 
-sequelize.authenticate()
-    .then(() => {
-        console.log('Conexión exitosa a la base de datos');
-    })
-    .catch(err => {
-        console.error('Error al conectar a la base de datos:', err);
-    });
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Conexión exitosa a la base de datos");
+  })
+  .catch((err) => {
+    console.error("Error al conectar a la base de datos:", err);
+  });
 
 module.exports = { sequelize };
